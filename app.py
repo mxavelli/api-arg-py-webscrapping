e4c95616215913cb8bbbe41057d6d357bda209c9
Moises Savelli <savellimoises@gmail.com>
2022-12-28 12:24:08 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/argcurrency')
+def arg_currency():
+    kvalues = []
+    keys = {'Compra', 'Venta'}
+    website = 'https://dolarhoy.com/i/cotizaciones/dolar-blue'
+    request_web = requests.get(website)
+    soup = BeautifulSoup(request_web.text, 'html.parser')
+    getdiv = soup.body.div
+    getp = getdiv.find_all('p')
+    for i in getp:
+        kvalues.append(i.get_text().replace('Compra', '').replace('Venta', ''))
+    values = zip(keys, kvalues)
+    finalValues = dict(values)
+    return json.dumps(finalValues)
+
+
+if __name__ == '__main__':
+    app.run()
