6f5f0e6f13f28d0fe205944fbe98331f631d0e17
Moises Savelli <savellimoises@gmail.com>
2022-12-29 13:08:40 -0300

    feature-12292022: add function insert_database, feature-12292022: add feature of insert information in database MySql, feature-12292022: add feature of datetime in Json data, feature-12292022: add feature of take os environment variables of database

--- app/app.py
+++ app/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
+from datetime import date
+import os
 import requests
 import json
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.getenv('DB_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('DB_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASS', 'hola05')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME', 'dollar_libre')
+
+mysql = MySQL(app)
 
 
 def get_arg_currency():
@@ -18,9 +28,22 @@ def get_arg_currency():
     return values
 
 
+def insert_database(jsonparam):
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO dollar_libre.dollar (json) VALUES (%s)", [jsonparam])
+        mysql.connection.commit()
+        return jsonparam
+    except Exception as e:
+        return {'Status': 'Error', 'Detail': str(e)}
+
+
 @app.route('/api/v1/argcurrency', methods=['GET'])
 def arg_currency():
-    return json.dumps(get_arg_currency())
+    jsonCreated = get_arg_currency()
+    jsonCreated['date'] = str(date.today())
+    jsonInserted = json.dumps(jsonCreated)
+    return insert_database(jsonInserted)
 
 
 @app.route('/api/v1/argcurrency/csv', methods=['GET'])
