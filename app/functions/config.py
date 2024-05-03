fd3cce6bec4013b196fefd1b17636786216598f2
Francis Rodriguez <francisngg@gmail.com>
2024-05-03 12:40:58 -0400

    feat: added PEN

--- app/functions/config.py
+++ app/functions/config.py
@@ -6,6 +6,7 @@ country_code_dict = {
     'VEN': 'VEN',
     'DOP_POPU': 'DOP_POPU',
     'DOP_BANRE': 'DOP_BANRE',
+    'PEN': 'PEN',
 }
 
 return_methods = {
@@ -21,4 +22,25 @@ country_code_label = {
     country_code_dict['VEN'] : 'Venezuela',
     country_code_dict['DOP_POPU'] : 'República Dominicana Banco Popular',
     country_code_dict['DOP_BANRE'] : 'República Dominicana Banreservas',
+    country_code_dict['PEN'] : 'Sol Peruano'
 }
+
+headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/108.0.0.0 '
+                          'Safari/537.36',
+            'user': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                    'application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept': 'application/json',
+            'accept-language': 'en,es-ES;q=0.9,es;q=0.8',
+            'pragma': 'no-cache',
+            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1'
+        }
